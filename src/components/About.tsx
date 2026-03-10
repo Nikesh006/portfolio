06fcb6eeefccf1dea639d0e0ef43d9e4c4f5513c
Nikesh006 <nikeshnpai3@email.com>
2026-03-10 22:30:51 +0530

    Fresh start without LFS

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          With 3+ years of experience, I have honed my skills in frontend and 
+          backend development, with a focus on animated 3D websites. I love 
+          solving problems and building things through code. Coding isn't just 
+          my profession - it is my passion.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,77 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:nikeshnpai3@gmail.com" data-cursor="disable">
+                nikeshnpai3@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>B.Tech in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/Nikesh006"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nikesh-n-pai"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://wa.me/qr/UHW47FB66DCBI1"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              WhatsApp <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/Nikesh006"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/_nik__esh_/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Nikesh N Pai</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
